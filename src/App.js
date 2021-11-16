31d8f0412e86ebb9274a05fda11fc216a5391a54
tal0311 <tal.amit0311@gmail.com>
2021-11-16 21:19:38 +0200

    pagination bug fixed

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ function App() {
   }, [loading, page])
 
   const handlePage = (index) => {
-    setPage(index + 1)
+    setPage(index)
   }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+  console.log()
 
   return (
     <main>
@@ -28,17 +48,23 @@ function App() {
         </div>
         {!loading && (
           <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              Prev
+            </button>
             {data.map((item, index) => {
               return (
                 <button
                   key={index}
-                  className='page-btn'
+                  className={`page-btn ${index === page ? 'active-btn' : null}`}
                   onClick={() => handlePage(index)}
                 >
                   {index + 1}
                 </button>
               )
             })}
+            <button className='next-btn' onClick={nextPage}>
+              Next
+            </button>
           </div>
         )}
       </section>
